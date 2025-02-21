84011961dd3bdd682baa67ca9dd1ad184f5f9808
Vyacheslav <slava.osinka@mail.ru>
2025-02-21 17:05:02 +0300

    Comments

--- src/main/java/com/project/Web_Project/configs/Config.java
+++ src/main/java/com/project/Web_Project/configs/Config.java
@@ -2,7 +2,8 @@ package com.project.Web_Project.configs;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-
+//base annotated spring config
+//no usages now, but I'm going to do them.
 @Configuration
 @ComponentScan("com.project.Web_Project")
 public class Config {
