7b81bdb17a21b9fe7195db4a261183507f26de6e
Vyacheslav <slava.osinka@mail.ru>
2025-02-21 17:02:51 +0300

    Remove unusable import

--- src/main/java/com/project/Web_Project/controllers/MainPage.java
+++ src/main/java/com/project/Web_Project/controllers/MainPage.java
@@ -2,7 +2,6 @@ package com.project.Web_Project.controllers;
 
 import com.project.Web_Project.utils.User;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.SessionAttributes;
