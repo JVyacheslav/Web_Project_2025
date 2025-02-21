ec168831de5439ee705e9a86cb61ef7d562cc91d
Vyacheslav <slava.osinka@mail.ru>
2025-02-21 17:03:43 +0300

    Comments

--- src/main/java/com/project/Web_Project/controllers/Registration/Registration.java
+++ src/main/java/com/project/Web_Project/controllers/Registration/Registration.java
@@ -6,6 +6,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 
+//Registration class - there is base form for user object.
 @Controller
 @RequestMapping("/reg")
 public class Registration {
